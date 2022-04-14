7cded7104d654434b53dc3189424cc0d1da7b9a4
alexanderobukhov <alexander.obukhov28@gmail.com>
2022-04-14 16:54:05 +0300

    add openapi and README.MD

--- src/main/kotlin/com/ao/wallet/WalletHistoryApplication.kt
+++ src/main/kotlin/com/ao/wallet/WalletHistoryApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WalletAppApplication
+class WalletHistoryApplication
 
 fun main(args: Array<String>) {
-	runApplication<WalletAppApplication>(*args)
+	runApplication<WalletHistoryApplication>(*args)
 }
